2e4cf983439c981363a8d7fdd13668b1bae09d2b
nittta-w <w.nitta@support-inc.co.jp>
2025-11-17 17:23:03 +0900

    日付指定、場所指定fech

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 $(function(){
 	setDay();
-	// addEvent();
-	displayResult();
+	addEvent();
 
 	// イベント登録
 	function addEvent(){
@@ -16,12 +15,19 @@ $(function(){
 		})
 
 		$('.js-click-btn').on('click', async () => {
-
 			const selectDay = $('[name="select_day"]').val();
 			const selectPlace = $('[name="select_place"]:checked').val();
 			const selectitem = $('[name="select_item"]:checked').map(function(){
 				return $(this).val();
 			}).get();
+
+			// バリデーション
+			// if(!selectDay || !selectPlace || selectitem.length === 0){
+			// 	alert('選択していないものがあるよ！');
+			// }
+			if(!selectDay || !selectPlace){
+				alert('選択していないものがあるよ！');
+			}
 
 			console.log(selectDay, selectPlace, selectitem);
 
@@ -29,18 +35,13 @@ $(function(){
 
 			console.log(data);
 
-			// const lat = 37.949128891578425; // 新田真上の緯度
-			// const lon = 139.30167159084863; // 新田真上の経度
-
-			// const data = await fetchData(lat, lon);
-			// console.log('取得成功', data);
-
-			// displayResult(data);
+			displayResult(data);
 		})
 	}
 
+	// GASからfetch
 	async function fetchData(selectDay, selectPlace, selectitem){
-		const url = `https://script.google.com/macros/s/AKfycbxqfCYFXShNrIeVfJSNEru0qcCfivL84zfwdZioYWmeNnK-APoMd-_7B0Cg3lEc5G-iVw/exec?sheet=${selectPlace}` ;
+		const url = `https://script.google.com/macros/s/AKfycbyO4MNxRtbmTh0OZN7xaGTgrj4OQmdCHszR5SwMjPPCrzic1PeGmyB1JFviFo1WPqOQFg/exec?sheet=${selectPlace}&day=${selectDay}` ; // GAS側のデプロイでver変わったらURLの変更必須
 
 		try{
 			const res = await fetch(url); // ① APIのURLを作成してアクセス → APIから返ってきた生のデータ（JSON形式の文字列）が res に入る
@@ -53,34 +54,32 @@ $(function(){
 		}
 	}
 
-	// 
+	// 選択可能なカレンダーの範囲をセット
 	function setDay(){
-		
+		const today = new Date().toISOString().split('T')[0];
+		const minDate = '2025-11-14';
 
+		$('#js-select-day').attr('min', minDate);
+		$('#js-select-day').attr('max', today);
 	}
 
 	// データ表示
-	function displayResult(){
+	function displayResult(data){
 
         let lineCtx = document.getElementById("chart");
         // 線グラフの設定
         let lineConfig = {
           type: 'line',
           data: {
-            // ※labelとデータの関係は得にありません
-            labels: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+            labels: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'],
             datasets: [{
               label: 'Red',
-              data: [20, 35, 40, 30, 45, 35, 40, 40, 30, 45, 35, 40, 40],
+              data: [data[0].気温, data[1].気温, data[2].気温, data[3].気温, data[4].気温, data[5].気温, data[6].気温, data[7].気温, data[8].気温, data[9].気温, data[10].気温, data[11].気温, data[12].気温, data[13].気温, data[14].気温, data[15].気温, data[16].気温, data[17].気温, data[18].気温, data[19].気温, data[20].気温, data[21].気温, data[22].気温, data[23].気温,],
               borderColor: '#f88',
             }, {
               label: 'Green',
-              data: [20, 15, 30, 25, 30, 40, 35, 40, 30, 45, 35, 40, 45],
+              data: [data[0].湿度, data[1].湿度, data[2].湿度, data[3].湿度, data[4].湿度, data[5].湿度, data[6].湿度, data[7].湿度, data[8].湿度, data[9].湿度, data[10].湿度, data[11].湿度, data[12].湿度, data[13].湿度, data[14].湿度, data[15].湿度, data[16].湿度, data[17].湿度, data[18].湿度, data[19].湿度, data[20].湿度, data[21].湿度, data[22].湿度, data[23].湿度,],
               borderColor: '#484',
-            }, {
-              label: 'Blue',
-              data: [1000, 2000, 1400, 2400, 2500, 2200, 2020, 1098, 2761, 1000, 2030, 2000, 2080],
-              borderColor: '#48f',
             }],
           },
           options: {
@@ -88,7 +87,7 @@ $(function(){
               // Y軸の最大値・最小値、目盛りの範囲などを設定する
               y: {
                 suggestedMin: 0,
-                suggestedMax: 60,
+                suggestedMax: 100,
                 ticks: {
                   stepSize: 20,
                 }
