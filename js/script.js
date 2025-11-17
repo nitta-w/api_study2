eb4b9d70d9a37836a5e95e55ee70d797cae5daf5
nittta-w <w.nitta@support-inc.co.jp>
2025-11-17 12:09:02 +0900

    見た目、JSデータ取得まで

--- js/script.js
+++ js/script.js
@@ -3,32 +3,42 @@ $(function(){
 
 	// イベント登録
 	function addEvent(){
-		$('.js-click-btn1').on('click', async ()=> {
-			const lat = 37.949128891578425; // 新田真上の緯度
-			const lon = 139.30167159084863; // 新田真上の経度
+		$('[name="select_item"]').on('change', function(e) {
+			const checked = $('[name="select_item"]:checked');
 
-			const data = await fetchData(lat, lon);
-			console.log('取得成功', data);
-
-			displayResult(data);
+			if(checked.length > 2){
+				alert('選べるのは2つまでだよ！');
+				$(this).prop('checked', false);
+				return;
+			}
 		})
 
-		$('.js-click-btn2').on('click', async ()=> {
-			const lat = 35.64238097186191; // 恵比寿真上の緯度
-			const lon = 139.7133929100686; // 恵比寿真上の経度
+		$('.js-click-btn').on('click', async () => {
 
-			const data = await fetchData(lat, lon);
-			console.log('取得成功', data);
+			const selectDay = $('[name="select_day"]').val();
+			const selectPlace = $('[name="select_place"]:checked').val();
+			const selectitem = $('[name="select_item"]:checked').map(function(){
+				return $(this).val();
+			}).get();
 
-			displayResult(data);
-			const $resultName = $('.js-result-name'); // 恵比寿のデータの地域名が三田だったので恵比寿に上書き
-			$resultName.text(`今の恵比寿の天気`);	
+			console.log(selectDay, selectPlace, selectitem);
+
+			const data = await fetchData(selectDay, selectPlace, selectitem);
+
+			console.log(data);
+
+			// const lat = 37.949128891578425; // 新田真上の緯度
+			// const lon = 139.30167159084863; // 新田真上の経度
+
+			// const data = await fetchData(lat, lon);
+			// console.log('取得成功', data);
+
+			// displayResult(data);
 		})
 	}
 
-	async function fetchData(lat, lon){
-		const apiKey = 'aa7a73d4e1adaa457a3c2150d7604db3';
-		const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=ja` ;
+	async function fetchData(selectDay, selectPlace, selectitem){
+		const url = `https://script.google.com/macros/s/AKfycbxqfCYFXShNrIeVfJSNEru0qcCfivL84zfwdZioYWmeNnK-APoMd-_7B0Cg3lEc5G-iVw/exec?sheet=${selectPlace}` ;
 
 		try{
 			const res = await fetch(url); // ① APIのURLを作成してアクセス → APIから返ってきた生のデータ（JSON形式の文字列）が res に入る
@@ -60,9 +70,3 @@ $(function(){
 	}
 
 });
-
-
-// GET で取得する場合、URLにパラメータをつけて取得する。パラメーターなので、全てリクエストが見える。秘密情報は送れない
-// (OpenWeather側はGETしか受け付けない)
-// POST でリクエストを送る場合は、パラメータとして送るわけではないのでリクエストが見えない。秘密の情報を送る時はこちらからリクエストする
-// GETもPOSTもどちらもリクエストを送ってリクエストに応じた返答を取得できるけど、リクエストの送り方が違う
